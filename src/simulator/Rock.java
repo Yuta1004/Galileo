5091e16ded595dc46e4d8874d78a4b74b64d3669
NakagamiYuta <guguru0014@gmail.com>
2020-03-01 14:17:38 +0900

    [change] コンストラクタ変更

--- src/simulator/Rock.java
+++ src/simulator/Rock.java
@@ -1,25 +1,24 @@
 public class Rock {
 
     private double x, y;
+    private final double v0, theta;
     private static double G = 9.8;
     private static double PI = 3.141592653589793;
 
-    /**
-     * コンストラクタ
-     */
-    public Rock() {
-        this(0, 0);
-    }
 
     /**
-     * コンストラクタ(初期座標指定)
+     * コンストラクタ
      *
      * @param x x座標の初期値
      * @param y y座標の初期値
+     * @param v0 射出速度
+     * @param theta 射出角度
      */
-    public Rock(double x, double y) {
+    public Rock(double x, double y, double v0, double theta) {
         this.x = x;
         this.y = y;
+        this.v0 = v0;
+        this.theta = theta;
     }
 
     /**
