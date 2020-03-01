63fbb1a3e93c0cc0fd3cace748423025fd5430a5
NakagamiYuta <guguru0014@gmail.com>
2020-03-01 18:53:12 +0900

    [change] とりあえず実行できるようにした

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-public class Main  {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
+
+import java.io.IOException;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Volcanic Rock Simulator");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            // 画面サイズ取得
+            Rectangle2D d = Screen.getPrimary().getVisualBounds();
+            int width = (int)Math.min(1280, d.getWidth());
+            int height = (int)Math.min(720, d.getHeight());
+
+            // Scene初期化
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainUI.fxml"));
+            Scene scene = new Scene(loader.load(), width, height);
+
+            // Stage初期化
+            stage.setTitle("Volcanic Rock Simulator");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
