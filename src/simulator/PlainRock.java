61431d713f72a6ef37647759332d92453711a2b1
NakagamiYuta <guguru0014@gmail.com>
2020-03-01 14:30:31 +0900

    [add] PlainRockクラス定義

---
+++ src/simulator/PlainRock.java
@@ -0,0 +1,19 @@
+public class PlainRock extends Rock {
+
+    /**
+     * コンストラクタ
+     *
+     * @param x x座標の初期値
+     * @param y y座標の初期値
+     * @param v0 射出速度
+     * @param theta 射出角度
+     */
+    public PlainRock(double x, double y, double v0, double theta) {
+        super(x, y, v0, theta);
+    }
+
+    @Override
+    public void move() {
+    }
+
+}
