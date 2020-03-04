f92567f0b814b9adad23a242435fb7e9a01a405a
NakagamiYuta <guguru0014@gmail.com>
2020-03-04 22:48:15 +0900

    [add] GenRockController定義

---
+++ src/controller/GenRockController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GenRockController implements Initializable {
+
+
+    /**
+     * 初期化
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resource) {
+    }
+
+}
