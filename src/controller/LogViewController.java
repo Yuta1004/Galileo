9715433b3b94987fc75e92dea4ac5bc19e90dd9f
NakagamiYuta <guguru0014@gmail.com>
2020-03-30 23:49:55 +0900

    [add] ログ表示の初期化を行うようにした

--- src/controller/LogViewController.java
+++ src/controller/LogViewController.java
@@ -2,14 +2,35 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LogViewController implements Initializable {
 
+    // ログ保持用
+    private int logCnt;
+    private ObservableList<String> logData;
+
+    // UI部品
+    @FXML
+    private ListView<String> logList;
+
+    /**
+     * ウィンドウ初期化
+     *
+     * @param location URL
+     * @param resource リソースデータ
+     */
     @Override
     public void initialize(URL location, ResourceBundle resource) {
+        // ログ表示初期化
+        logCnt = 0;
+        logData = FXCollections.observableArrayList();
+        logList.setItems(logData);
     }
 
 }
