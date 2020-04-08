f7d31103bfc49323700e3b76749bf33b9cb44769
NakagamiYuta <guguru0014@gmail.com>
2020-04-08 21:46:51 +0900

    [add] なんちゃってDB作成

---
+++ src/db/Settings.java
@@ -0,0 +1,15 @@
+package db;
+
+/**
+ * シミュレータについての設定情報をもつ
+ * DB...?
+ */
+public class Settings {
+
+    public static double RockMagnification = 1.0;
+    public static boolean ViewRatioNormalize = false;
+    public static double Step = 0.1;
+    public static double Step10 = 1.0;
+    // public static double ConstantDrag = 0.2;
+
+}
