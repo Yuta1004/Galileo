da05289e056878a8020631333df76033920e711f
NakagamiYuta <guguru0014@gmail.com>
2020-04-08 21:53:30 +0900

    [add] 表示比正規化の設定を行えるようにした

--- src/controller/AdvancedSettingsController.java
+++ src/controller/AdvancedSettingsController.java
@@ -1,14 +1,25 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 
 import java.util.ResourceBundle;
 import java.net.URL;
 
+import db.Settings;
+
 public class AdvancedSettingsController implements Initializable {
+
+    @FXML
+    private CheckBox viewRatioNormalize;
 
     @Override
     public void initialize(URL location, ResourceBundle resource) {
+        // 表示比正規化
+        viewRatioNormalize.setOnAction(event -> {
+            Settings.ViewRatioNormalize = viewRatioNormalize.isSelected();
+        });
     }
 
 }
