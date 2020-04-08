7d7fc175c194e0357eb544d95faedc0bf407de3f
NakagamiYuta <guguru0014@gmail.com>
2020-04-08 22:01:44 +0900

    [add] 詳細設定の噴石拡大表示を有効にした(反映なし)

--- src/controller/AdvancedSettingsController.java
+++ src/controller/AdvancedSettingsController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Slider;
 
 import java.util.ResourceBundle;
 import java.net.URL;
@@ -11,11 +12,19 @@ import db.Settings;
 
 public class AdvancedSettingsController implements Initializable {
 
+    // UI部品
+    @FXML
+    private Slider rockMagValue;
     @FXML
     private CheckBox viewRatioNormalize;
 
     @Override
     public void initialize(URL location, ResourceBundle resource) {
+        // 噴石拡大表示
+        rockMagValue.valueProperty().addListener((obs, oldVal, newVal) -> {
+            Settings.RockMagnification = newVal.doubleValue();
+            System.out.println(Settings.RockMagnification);
+        });
         // 表示比正規化
         viewRatioNormalize.setOnAction(event -> {
             Settings.ViewRatioNormalize = viewRatioNormalize.isSelected();
