f674c421f656d883d5c66487a4adfe4c2a62237a
NakagamiYuta <guguru0014@gmail.com>
2020-04-08 21:33:24 +0900

    [add] AdvancedSettingsController

---
+++ src/controller/AdvancedSettingsController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.util.ResourceBundle;
+import java.net.URL;
+
+public class AdvancedSettingsController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resource) {
+    }
+
+}
