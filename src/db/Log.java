a5684b4bc7be03c811c1b090f015b6a2cb0b647d
NakagamiYuta <guguru0014@gmail.com>
2020-04-09 22:05:44 +0900

    [change] ログ番号を振る処理を変更

--- src/db/Log.java
+++ src/db/Log.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 
 public class Log {
 
-    private static int num;
     private static boolean updatedFlag = false;
     private static ArrayList<String> logData = new ArrayList<String>();
 
     public static void addData(String log) {
         updatedFlag = true;
-        logData.add(num+": "+log);
+        logData.add(logData.size()+": "+log);
     }
 
     public static boolean checkUpdate() {
