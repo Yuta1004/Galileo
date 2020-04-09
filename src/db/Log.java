70dc109335cf69c565ae514ab53fe6b72cc09b54
NakagamiYuta <guguru0014@gmail.com>
2020-04-09 22:07:39 +0900

    [add] Logにコメントを追加

--- src/db/Log.java
+++ src/db/Log.java
@@ -7,11 +7,21 @@ public class Log {
     private static boolean updatedFlag = false;
     private static ArrayList<String> logData = new ArrayList<String>();
 
+    /**
+     * ログを追加する
+     *
+     * @param log ログ
+     */
     public static void addData(String log) {
         updatedFlag = true;
         logData.add(logData.size()+": "+log);
     }
 
+    /**
+     * 更新があったかどうかを返す
+     *
+     * @return boolean
+     */
     public static boolean checkUpdate() {
         return updatedFlag;
     }
