05cbb43099d5ac8ed8fe7bc27823454172628b4d
NakagamiYuta <guguru0014@gmail.com>
2020-04-12 23:02:50 +0900

    [change] 噴石表示比の正規化はデフォルトでONにした

--- src/db/Settings.java
+++ src/db/Settings.java
@@ -7,7 +7,7 @@ package db;
 public class Settings {
 
     public static double RockMagnification = 1.0;
-    public static boolean ViewRatioNormalize = false;
+    public static boolean ViewRatioNormalize = true;
     public static boolean AxisNormalize = false;
     public static double StepVal = 0.1;
     // public static double ConstantDrag = 0.2;
